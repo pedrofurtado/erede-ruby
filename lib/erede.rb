162deb82961697f2fa6f4fd92275a3ae5fa29ee4
Pedro Furtado <pedro.furtado@locaweb.com.br>
2022-05-21 00:21:22 -0300

    Subindo arquivos que faltam para completar o comportamento da gem

--- lib/erede.rb
+++ lib/erede.rb
@@ -1,30 +1,28 @@
-require "erede/service/create_transaction_service"
-require "erede/service/capture_transaction_service"
-require "erede/service/cancel_transaction_service"
-require "erede/service/get_transaction_service"
-require "erede/version"
-require "erede/iata.rb"
-require "erede/authorization.rb"
-require "erede/document.rb"
-require "erede/antifraud.rb"
-require "erede/threedsecure.rb"
-require "erede/capture.rb"
-require "erede/consumer.rb"
-require "erede/transaction.rb"
-require "erede/submerchant.rb"
-require "erede/item.rb"
-require "erede/phone.rb"
-require "erede/url.rb"
-require "erede/refund.rb"
-require "erede/version.rb"
-require "erede/environment.rb"
-require "erede/store.rb"
-require "erede/passenger.rb"
-require "erede/flight.rb"
-require "erede/address.rb"
-require "erede/cart.rb"
-require "erede.rb"
-require "json"
+require 'erede/transaction_service'
+require 'erede/create_transaction_service'
+require 'erede/capture_transaction_service'
+require 'erede/cancel_transaction_service'
+require 'erede/get_transaction_service'
+require 'erede/version'
+require 'erede/iata'
+require 'erede/authorization'
+require 'erede/document'
+require 'erede/threedsecure'
+require 'erede/capture'
+require 'erede/consumer'
+require 'erede/transaction'
+require 'erede/submerchant'
+require 'erede/item'
+require 'erede/phone'
+require 'erede/url'
+require 'erede/refund'
+require 'erede/environment'
+require 'erede/store'
+require 'erede/passenger'
+require 'erede/flight'
+require 'erede/address'
+require 'erede/cart'
+require 'json'
 
 module EREDE
   USER_AGENT = 'eRede/1.0 (SDK Ruby)'
@@ -32,7 +30,6 @@ module EREDE
   class ERede
     attr_accessor :store
 
-    # @param store [Store]
     def initialize(store)
       @store = store
     end
@@ -42,43 +39,43 @@ module EREDE
     end
 
     def create(transaction)
-      service = EREDE::SERVICE::CreateTransactionService.new(@store, transaction)
+      service = CreateTransactionService.new(@store, transaction)
 
-      service.execute()
+      service.execute
     end
 
     def cancel(transaction)
-      service = EREDE::SERVICE::CancelTransactionService.new(@store, transaction)
+      service = CancelTransactionService.new(@store, transaction)
 
-      service.execute()
+      service.execute
     end
 
     def capture(transaction)
-      service = EREDE::SERVICE::CaptureTransactionService.new(@store, transaction)
+      service = CaptureTransactionService.new(@store, transaction)
 
-      service.execute()
+      service.execute
     end
 
     def get_by_id(tid)
-      service = EREDE::SERVICE::GetTransactionService.new(@store)
+      service = GetTransactionService.new(@store)
       service.tid = tid
 
-      service.execute()
+      service.execute
     end
 
     def get_by_reference(reference)
-      service = EREDE::SERVICE::GetTransactionService.new(@store)
+      service = GetTransactionService.new(@store)
       service.reference = reference
 
-      service.execute()
+      service.execute
     end
 
     def get_refunds(tid)
-      service = EREDE::SERVICE::GetTransactionService.new(@store)
+      service = GetTransactionService.new(@store)
       service.tid = tid
-      service.refund = true
+      service.refunds = true
 
-      service.execute()
+      service.execute
     end
 
     def zero(transaction)
