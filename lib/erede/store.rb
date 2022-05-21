a7fb71fa7c301aa63958f2b0ecef8cb233e2ae34
Pedro Furtado <pedro.furtado@locaweb.com.br>
2022-05-21 00:21:53 -0300

    Subindo arquivos que faltam para completar o comportamento da gem

--- lib/erede/store.rb
+++ lib/erede/store.rb
@@ -1,24 +1,24 @@
 module EREDE
   class Store
     attr_accessor :token,
-                  :pv,
+                  :filiation,
                   :environment
 
     # @param token [String] Token
-    # @param pv [String] PV
+    # @param filiation [String] filiation
     # @param environment [Environment] The environment
-    def initialize(token, pv, environment = nil)
+    def initialize(token, filiation, environment = nil)
       environment ||= Environment.production
 
       @token = token
-      @pv = pv
+      @filiation = filiation
       @environment = environment
     end
 
     def to_json(*)
       {
         token: @token,
-        pv: @pv
+        filiation: @filiation
       }.delete_if { |k, v| v.nil? }.to_json
     end
   end
