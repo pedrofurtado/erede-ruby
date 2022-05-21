5c303a54b80af08fe9b3f2131f38f17880e52558
Pedro Furtado <pedro.furtado@locaweb.com.br>
2022-05-21 00:21:43 -0300

    Subindo arquivos que faltam para completar o comportamento da gem

--- lib/erede/environment.rb
+++ lib/erede/environment.rb
@@ -1,15 +1,15 @@
 module EREDE
   class Environment
-    PRODUCTION = "https://api.userede.com.br/erede"
-    SANDBOX = "https://api.userede.com.br/desenvolvedores"
-    VERSION = "v1"
+    PRODUCTION = 'https://api.userede.com.br/erede'
+    SANDBOX    = 'https://api.userede.com.br/desenvolvedores'
+    VERSION    = 'v1'
 
     attr_accessor :ip,
                   :session_id,
                   :endpoint
 
     def initialize(endpoint, version = Environment::VERSION)
-      @endpoint = "#{endpoint}/#{version}/"
+      @endpoint = "#{endpoint}/#{version}"
     end
 
     def self.production
@@ -21,11 +21,11 @@ module EREDE
     end
 
     def to_json(*)
-      { consumer:
-          {
-            ip: @ip,
-            sessionId: @session_id
-          }
+      {
+        consumer: {
+          ip: @ip,
+          sessionId: @session_id
+        }
       }.delete_if { |k, v| v.nil? }.to_json
     end
   end
